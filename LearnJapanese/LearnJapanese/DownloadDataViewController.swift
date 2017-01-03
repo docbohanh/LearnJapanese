0447a40b5ab126f030112189b3687a51edb2c225
lighthouse2403 <haidang2403@gmail.com>
2017-01-03 18:02:19 +0700

    no message

--- LearnJapanese/LearnJapanese/DownloadDataViewController.swift
+++ LearnJapanese/LearnJapanese/DownloadDataViewController.swift
@@ -57,7 +57,7 @@ class DownloadDataViewController: UIViewController {
     }
     
     func saveDataToDatabase(response : DataResponse<Any>) {
-        DispatchQueue.main.async {
+//        DispatchQueue.main.async {
             
         if response.result.value != nil {
         let value = response.result.value as! [String:AnyObject]
@@ -73,92 +73,174 @@ class DownloadDataViewController: UIViewController {
             }
             let resultDictionary = response.result.value! as! [String:AnyObject]
             let dictionaryArray = resultDictionary["Data"] as! [[String : AnyObject]]
-            for index in 0..<dictionaryArray.count {
+                MagicalRecord.save({(localContext) in
+                    for index in 0..<dictionaryArray.count {
 
-            let localContext = NSManagedObjectContext.mr_default()
-            localContext.mr_save({localContext in
-                        let word = dictionaryArray[index]
+                    let word = dictionaryArray[index]
+
+                    let wordData = Translate.mr_createEntity(in:localContext)
+                    if let word_id = word["Id"] {
+                        wordData?.id = String(describing: word_id)
+                    }
+                    if let Word = word["Word"] {
+                        wordData?.word = Word as? String
+                    }
                     
-                        let wordData = Translate.mr_createEntity()
-                        if let word_id = word["Id"] {
-                            wordData?.id = String(describing: word_id)
-                        }
-                        if let Word = word["Word"] {
-                            wordData?.word = Word as? String
-                        }
-                        
-                        if let kana = word["Kana"] {
-                            wordData?.kana = kana as? String
-                        }
-                        if let Romaji = word["Romaji"] {
-                            wordData?.romaji = Romaji["Romaji"] as? String
-                        }
-                        if let SoundUrl = word["SoundUrl"] {
-                            wordData?.sound_url = SoundUrl["SoundUrl"] as? String
-                        }
-                        if let LastmodifiedDate = word["LastmodifiedDate"] {
-                            let trimString = LastmodifiedDate
-                            let timeStamp:String = trimString.substring(from: 5)
-                            wordData?.last_modified = timeStamp.substring(to: (timeStamp.characters.count - 7))
-                        }
-                        if let Modified = word["Modified"] {
-                            wordData?.modified = Modified as? String
-                        }
-                        if let SoundUrl = word["SoundUrl"] {
-                            wordData?.sound_url = SoundUrl as? String
-                        }
-                        if let Avatar = word["Avatar"] {
-                            wordData?.avatar = Avatar as? String
-                        }
-                        
-                        let meaningWord = word["Meaning"] as? [[String:AnyObject]]
-                        let first = meaningWord?.first
-                        if let Meaning  = first?["Meaning"] {
-                            wordData?.meaning_name = Meaning as? String
-                        }
-                        if let MeaningId = first?["MeaningId"] {
-                            wordData?.meaningId = MeaningId as? String
-                        }
-                        if let Type = first?["Type"] {
-                            wordData?.meaning_type = String(describing: Type)
-                        }
-                        
-                        let exampleWord = word["Example"] as? [[String:AnyObject]]
-                        let firstExample = exampleWord?.first
-
-                        if let ExampleId = firstExample?["ExampleId"] {
-                            wordData?.example_id = String(describing: ExampleId)
-                        }
-                        if let Example = firstExample?["Example"] {
-                            wordData?.example_name = Example as? String
-                        }
-                        if let Meaning = firstExample?["Meaning"] {
-                            wordData?.example_meaning_name = Meaning as? String
-                        }
-                        if let MeaningId = firstExample?["MeaningId"] {
-                            wordData?.example_meaning_id = String(describing: MeaningId)
-                        }
-                        if let Romaji = firstExample?["Romaji"] {
-                            wordData?.example_romaji = Romaji as? String
-                        }
-                        if let Kana = firstExample?["Kana"] {
-                            wordData?.example_kana = Kana as? String
-                        }
-                        if let SoundUrl = firstExample?["SoundUrl"] {
-                            wordData?.example_sound_url = SoundUrl as? String
-                        }
-                    self.appDelegate.wordArray.append(wordData!)
-                        self.checkProgress()
-                        print("Dang luu du lieu ")
+                    if let kana = word["Kana"] {
+                        wordData?.kana = kana as? String
+                    }
+                    if let Romaji = word["Romaji"] {
+                        wordData?.romaji = Romaji["Romaji"] as? String
+                    }
+                    if let SoundUrl = word["SoundUrl"] {
+                        wordData?.sound_url = SoundUrl["SoundUrl"] as? String
+                    }
+                    if let LastmodifiedDate = word["LastmodifiedDate"] {
+                        let trimString = LastmodifiedDate
+                        let timeStamp:String = trimString.substring(from: 5)
+                        wordData?.last_modified = timeStamp.substring(to: (timeStamp.characters.count - 7))
+                    }
+                    if let Modified = word["Modified"] {
+                        wordData?.modified = Modified as? String
+                    }
+                    if let SoundUrl = word["SoundUrl"] {
+                        wordData?.sound_url = SoundUrl as? String
+                    }
+                    if let Avatar = word["Avatar"] {
+                        wordData?.avatar = Avatar as? String
+                    }
                     
-                    print("Dang luu du lieu ")
-
+                    let meaningWord = word["Meaning"] as? [[String:AnyObject]]
+                    let first = meaningWord?.first
+                    if let Meaning  = first?["Meaning"] {
+                        wordData?.meaning_name = Meaning as? String
+                    }
+                    if let MeaningId = first?["MeaningId"] {
+                        wordData?.meaningId = MeaningId as? String
+                    }
+                    if let Type = first?["Type"] {
+                        wordData?.meaning_type = String(describing: Type)
+                    }
+                    
+                    let exampleWord = word["Example"] as? [[String:AnyObject]]
+                    let firstExample = exampleWord?.first
+                    
+                    if let ExampleId = firstExample?["ExampleId"] {
+                        wordData?.example_id = String(describing: ExampleId)
+                    }
+                    if let Example = firstExample?["Example"] {
+                        wordData?.example_name = Example as? String
+                    }
+                    if let Meaning = firstExample?["Meaning"] {
+                        wordData?.example_meaning_name = Meaning as? String
+                    }
+                    if let MeaningId = firstExample?["MeaningId"] {
+                        wordData?.example_meaning_id = String(describing: MeaningId)
+                    }
+                    if let Romaji = firstExample?["Romaji"] {
+                        wordData?.example_romaji = Romaji as? String
+                    }
+                    if let Kana = firstExample?["Kana"] {
+                        wordData?.example_kana = Kana as? String
+                    }
+                    if let SoundUrl = firstExample?["SoundUrl"] {
+                        wordData?.example_sound_url = SoundUrl as? String
+                    }
                 
-            }, completion: { contextDidSave in
-                //saving is successful
-                print("saving is successful")
-            })
+                    self.appDelegate.wordArray.append(wordData!)
+                    self.checkProgress()
+                    print("Dang luu du lieu ")
             }
+
+                }, completion: {(contextDidSave,error) in
+                    print("saving is successful")
+
+                })
+                
+//            let localContext = NSManagedObjectContext.mr_default()
+//            localContext.mr_save({localContext in
+//                        let word = dictionaryArray[index]
+//                    
+//                        let wordData = Translate.mr_createEntity()
+//                        if let word_id = word["Id"] {
+//                            wordData?.id = String(describing: word_id)
+//                        }
+//                        if let Word = word["Word"] {
+//                            wordData?.word = Word as? String
+//                        }
+//                        
+//                        if let kana = word["Kana"] {
+//                            wordData?.kana = kana as? String
+//                        }
+//                        if let Romaji = word["Romaji"] {
+//                            wordData?.romaji = Romaji["Romaji"] as? String
+//                        }
+//                        if let SoundUrl = word["SoundUrl"] {
+//                            wordData?.sound_url = SoundUrl["SoundUrl"] as? String
+//                        }
+//                        if let LastmodifiedDate = word["LastmodifiedDate"] {
+//                            let trimString = LastmodifiedDate
+//                            let timeStamp:String = trimString.substring(from: 5)
+//                            wordData?.last_modified = timeStamp.substring(to: (timeStamp.characters.count - 7))
+//                        }
+//                        if let Modified = word["Modified"] {
+//                            wordData?.modified = Modified as? String
+//                        }
+//                        if let SoundUrl = word["SoundUrl"] {
+//                            wordData?.sound_url = SoundUrl as? String
+//                        }
+//                        if let Avatar = word["Avatar"] {
+//                            wordData?.avatar = Avatar as? String
+//                        }
+//                        
+//                        let meaningWord = word["Meaning"] as? [[String:AnyObject]]
+//                        let first = meaningWord?.first
+//                        if let Meaning  = first?["Meaning"] {
+//                            wordData?.meaning_name = Meaning as? String
+//                        }
+//                        if let MeaningId = first?["MeaningId"] {
+//                            wordData?.meaningId = MeaningId as? String
+//                        }
+//                        if let Type = first?["Type"] {
+//                            wordData?.meaning_type = String(describing: Type)
+//                        }
+//                        
+//                        let exampleWord = word["Example"] as? [[String:AnyObject]]
+//                        let firstExample = exampleWord?.first
+//
+//                        if let ExampleId = firstExample?["ExampleId"] {
+//                            wordData?.example_id = String(describing: ExampleId)
+//                        }
+//                        if let Example = firstExample?["Example"] {
+//                            wordData?.example_name = Example as? String
+//                        }
+//                        if let Meaning = firstExample?["Meaning"] {
+//                            wordData?.example_meaning_name = Meaning as? String
+//                        }
+//                        if let MeaningId = firstExample?["MeaningId"] {
+//                            wordData?.example_meaning_id = String(describing: MeaningId)
+//                        }
+//                        if let Romaji = firstExample?["Romaji"] {
+//                            wordData?.example_romaji = Romaji as? String
+//                        }
+//                        if let Kana = firstExample?["Kana"] {
+//                            wordData?.example_kana = Kana as? String
+//                        }
+//                        if let SoundUrl = firstExample?["SoundUrl"] {
+//                            wordData?.example_sound_url = SoundUrl as? String
+//                        }
+//                    self.appDelegate.wordArray.append(wordData!)
+//                        self.checkProgress()
+//                        print("Dang luu du lieu ")
+//                    
+//                    print("Dang luu du lieu ")
+//
+//                
+//            }, completion: { contextDidSave in
+//                //saving is successful
+//                print("saving is successful")
+//            })
+            
             //reload TableView
         } else {
             ProjectCommon.initAlertView(viewController: self, title: "", message: "Không có phiên bản mới", buttonArray: ["Cancel"], onCompletion: {_ in
@@ -169,7 +251,7 @@ class DownloadDataViewController: UIViewController {
             print("can't get word")
         }
             }
-    }
+//    }
     }
 
     func checkProgress() {
