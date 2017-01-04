673b560293574466bb7ad259b4922db4e7b153a0
Thành Lã <huuthanhla@gmail.com>
2017-01-05 01:30:58 +0700

    Update DownloadDataViewController.swift

--- LearnJapanese/LearnJapanese/DownloadDataViewController.swift
+++ LearnJapanese/LearnJapanese/DownloadDataViewController.swift
@@ -64,6 +64,7 @@ class DownloadDataViewController: UIViewController {
     func saveDataToDatabase(response : DataResponse<Any>) {
         //        DispatchQueue.main.async {
         
+        ///Thành Lã: 2017/01/05
         guard let value = response.result.value as? [String:AnyObject] else { return }
         
         guard response.result.error == nil,
