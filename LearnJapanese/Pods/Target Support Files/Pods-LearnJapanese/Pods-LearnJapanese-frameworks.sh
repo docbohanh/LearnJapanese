6da60628bdcdd6eb90d354160b86996c73799f03
lighthouse2403 <haidang2403@gmail.com>
2017-01-11 22:23:44 +0700

    no message

--- LearnJapanese/Pods/Target Support Files/Pods-LearnJapanese/Pods-LearnJapanese-frameworks.sh
+++ LearnJapanese/Pods/Target Support Files/Pods-LearnJapanese/Pods-LearnJapanese-frameworks.sh
@@ -84,18 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/FGTranslator/FGTranslator.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MagicalRecord/MagicalRecord.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/PINCache/PINCache.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SVProgressHUD/SVProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/FGTranslator/FGTranslator.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MagicalRecord/MagicalRecord.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/PINCache/PINCache.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SVProgressHUD/SVProgressHUD.framework"
 fi
